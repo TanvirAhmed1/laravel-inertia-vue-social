b014cf61d7b34748be7415084ebfefd0f6bc8947
Tanvir Ahmed <61121499+TanvirAhmed1@users.noreply.github.com>
2024-10-27 00:35:24 +0600

    Insta logo added to Layout

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import HeartOutline from 'vue-material-design-icons/HeartOutline.vue';
+import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
+import Compass from 'vue-material-design-icons/Compare.vue';
+import SendOutline from 'vue-material-design-icons/SendOutline.vue';
+import Plus from 'vue-material-design-icons/Plus.vue';
+import AccountOutline from 'vue-material-design-icons/AccountOutline.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+import AccountPlusOutline from 'vue-material-design-icons/AccountPlusOutline.vue'; 
+let showCreatePost = ref(false)
+</script>
+
+<template>
+    <div id="MainLayout" class="w-full h-screen">
+        <div v-if="$page.url === '/'" id="TopNavHome" class="fixed z-30 md:hidden flex items-center w-full bg-white h-[61px] border-b border-b-gray-300">
+            <Link href="/">
+                <img  class="w-[105px] ml-6 cursor-pointer" src="/insta-logo.png">
+            </Link>
+        </div>
+    </div>
+</template>
